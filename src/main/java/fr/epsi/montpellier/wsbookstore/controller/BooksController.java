6818b0f4487278d8d93ef70eefa405f4ba3b3f74
Rafayel Azizyan <rafayel.azizyan@gmail.com>
2021-02-22 20:54:11 +0100

    Exercice-1 ORM et Accéder à la BDD

--- src/main/java/fr/epsi/montpellier/wsbookstore/controller/BooksController.java
+++ src/main/java/fr/epsi/montpellier/wsbookstore/controller/BooksController.java
@@ -1,26 +1,30 @@
 package fr.epsi.montpellier.wsbookstore.controller;
 
 import fr.epsi.montpellier.wsbookstore.models.Book;
+import fr.epsi.montpellier.wsbookstore.repository.BooksRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
 
 
 @RestController
 @RequestMapping("/api")
 public class BooksController {
 
-    // http://localhost/api/books
+    private BooksRepository booksRepository;
+
+    public BooksController(BooksRepository booksRepository) {
+        this.booksRepository = booksRepository;
+    }
+
 
     @GetMapping("/books")
-    public List<Book> getBook() {
-        List<Book> list = new ArrayList<>();
-        list.add(new Book("123457", 1, "MobyDick" ));
-        list.add(new Book("1234589", 2, "Germinal"));
-        return list;
+    public Iterable<Book> getBooks() {
+        return booksRepository.findAll();
     }
+
+
+
 
 }
--- src/main/java/fr/epsi/montpellier/wsbookstore/models/Book.java
+++ src/main/java/fr/epsi/montpellier/wsbookstore/models/Book.java
@@ -1,15 +1,46 @@
 package fr.epsi.montpellier.wsbookstore.models;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
 public class Book {
 
+    @Id
     private String isbn;
     private long publisherId;
     private String title;
+    private Date publicationyear;
+    private double price;
+    private String format;
+    private String edition;
+    private long pages;
+    private String dimensions;
+    private String overview;
+    private String synopsis;
 
-  public Book(String isbn, long publisherId, String title) {
+  public Book() {
+  }
+
+  public Book(String isbn, long publisherId, String title, Date publicationyear, double price, String format, String edition, long pages, String dimensions, String overview, String synopsis) {
     this.isbn = isbn;
     this.publisherId = publisherId;
     this.title = title;
+    this.publicationyear = publicationyear;
+    this.price = price;
+    this.format = format;
+    this.edition = edition;
+    this.pages = pages;
+    this.dimensions = dimensions;
+    this.overview = overview;
+    this.synopsis = synopsis;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Book=[isbn:%s, title:%s, publisherId:%d]", isbn, title, publisherId);
   }
 
   public String getIsbn() {
@@ -38,4 +69,67 @@ public class Book {
     this.title = title;
   }
 
+  public Date getPublicationyear() {
+    return publicationyear;
+  }
+
+  public void setPublicationyear(Date publicationyear) {
+    this.publicationyear = publicationyear;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public String getFormat() {
+    return format;
+  }
+
+  public void setFormat(String format) {
+    this.format = format;
+  }
+
+  public String getEdition() {
+    return edition;
+  }
+
+  public void setEdition(String edition) {
+    this.edition = edition;
+  }
+
+  public long getPages() {
+    return pages;
+  }
+
+  public void setPages(long pages) {
+    this.pages = pages;
+  }
+
+  public String getDimensions() {
+    return dimensions;
+  }
+
+  public void setDimensions(String dimensions) {
+    this.dimensions = dimensions;
+  }
+
+  public String getOverview() {
+    return overview;
+  }
+
+  public void setOverview(String overview) {
+    this.overview = overview;
+  }
+
+  public String getSynopsis() {
+    return synopsis;
+  }
+
+  public void setSynopsis(String synopsis) {
+    this.synopsis = synopsis;
+  }
 }
