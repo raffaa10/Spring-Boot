f798f677a007d94b98f1fc6f589668cf6cfa0b42
Rafayel Azizyan <rafayel.azizyan@gmail.com>
2021-02-22 21:12:11 +0100

    Ex-3 CRUD - Create - avec la Validation des données

--- src/main/java/fr/epsi/montpellier/wsbookstore/controller/BooksController.java
+++ src/main/java/fr/epsi/montpellier/wsbookstore/controller/BooksController.java
@@ -3,11 +3,13 @@ package fr.epsi.montpellier.wsbookstore.controller;
 import fr.epsi.montpellier.wsbookstore.models.Book;
 import fr.epsi.montpellier.wsbookstore.repository.BooksRepository;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 
 @RestController
@@ -37,6 +39,29 @@ public class BooksController {
                         HttpStatus.NOT_FOUND,
                         String.format("Livre '%s' non trouvé", isbn))
                 );
+    }
+
+    @PostMapping("/books")
+    public ResponseEntity<Void> addBook(@Validated @RequestBody Book book) {
+        try {
+            Book bookCreated = booksRepository.save(book);
+            URI location = ServletUriComponentsBuilder
+                    .fromCurrentRequestUri()
+                    .path("/{id}")
+                    .buildAndExpand(bookCreated.getIsbn())
+                    .toUri();
+            return ResponseEntity.created(location).build();
+        } catch (Exception exception) {
+            LogError(exception);
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    String.format("Book: %s", book.toString())
+            );
+        }
+    }
+    private void LogError(Exception exception) {
+        System.err.printf("Error, Class=%s\n", this.getClass().getCanonicalName());
+        exception.printStackTrace(System.err);
     }
 
 
