5570a059d63cb55b39b2116544f54f389a3b9668
Rafayel Azizyan <rafayel.azizyan@gmail.com>
2021-02-23 18:42:46 +0100

    TP-2 Ex-extra-2 | Ajout de la clé étrangère

---
+++ src/main/java/fr/epsi/montpellier/wsbookstore/controller/CommanddetailsController.java
@@ -0,0 +1,43 @@
+package fr.epsi.montpellier.wsbookstore.controller;
+
+import fr.epsi.montpellier.wsbookstore.models.Command;
+import fr.epsi.montpellier.wsbookstore.models.Commanddetail;
+import fr.epsi.montpellier.wsbookstore.repository.CommanddetailsRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/api")
+public class CommanddetailsController {
+
+    private final CommanddetailsRepository commanddetailsRepository;
+
+    public CommanddetailsController(CommanddetailsRepository commanddetailsRepository) {
+        this.commanddetailsRepository = commanddetailsRepository;
+    }
+
+    @GetMapping("/commanddetails")
+    public Iterable<Commanddetail> getCommanddetails() {
+        return commanddetailsRepository.findAll();
+    }
+
+    @GetMapping("/commanddetails/{id}")
+    public Commanddetail getCommanddetails(@PathVariable(value = "id") Long id) {
+        return findById(id);
+    }
+
+    private Commanddetail findById(Long id) {
+        return commanddetailsRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        String.format("Commande '%s' non trouvée", id))
+                );
+    }
+
+
+
+}
---
+++ src/main/java/fr/epsi/montpellier/wsbookstore/repository/CommanddetailsRepository.java
@@ -0,0 +1,8 @@
+package fr.epsi.montpellier.wsbookstore.repository;
+
+import fr.epsi.montpellier.wsbookstore.models.Commanddetail;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CommanddetailsRepository extends CrudRepository<Commanddetail, Long> {
+
+}
